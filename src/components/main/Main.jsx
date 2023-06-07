dac0696420a4c800aadfdf1c76930e5014519df0
Sefatullah Omar <sefaullahomar1@gmail.com>
2023-06-07 08:46:24 -0700

    first

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./main.css"
+function Main() {
+  return (
+    <div className='main'>
+      <div className="container">
+        
+        <div className='logo'>
+          Logo
+        </div>
+
+        <div className='contents'>
+          <p>Home</p>
+          <p>Interactive AI</p>
+          <p>Software</p>
+          <p>Services</p>
+          <p>Learning</p>
+          <p>Blog</p>
+        </div>
+
+        <div className='subscribeButton'>
+          <button>Subscribe</button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Main
